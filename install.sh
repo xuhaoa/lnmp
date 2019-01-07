1d578723f93f02854ff7c0ec6ae54dc8ece84c6c
getbin <43870946+getbin@users.noreply.github.com>
2019-01-08 00:48:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,58 @@
-#coding:utf-8
+#!/usr/bin/env bash
+
+clear
+
+keyerrorlogo='
+================================================================================================================
+
+           --------- Python3.6.6 一键脚本 ----------
+		   
+	已在 CentOS7-Minimal-1804 上测试 成功,其他暂不支持！
+		   
+                                                 -- https://lnmp.me 
+================================================================================================================';
+echo "$keyerrorlogo";
+echo
+
+echo "按Enter开始安装,按Crtl+c取消........";
+read;
+
+#coding:utf-8 
 export LANG=UTF-8
 export LANGUAGE=UTF-8
 
+#更改yum源和epel源到阿里云
+yum -y install epel-release
 cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -P /etc/yum.repos.d/ http://mirrors.aliyun.com/repo/epel-7.repo
 yum clean all
 yum makecache
 
-yum -y update
+#更新并安装组件
+#yum -y update
 yum -y install gcc gcc-c++ 
 yum -y install python-devel.x86_64 libxml2-devel 
-yum -y install epel-release
-yum -y install nginx
+
+#安装Nginx ( https://segmentfault.com/a/1190000017781356 )yum install -y wget   
+rpm -ivh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+yum install -y nginx
 systemctl start nginx
 
-wget https://repo.mysql.com//mysql57-community-release-el7-11.noarch.rpm
-yum -y localinstall mysql57-community-release-el7-11.noarch.rpm
+#安装MySQL   ( https://blog.csdn.net/javandroid/article/details/81225839 )
+rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 yum -y install mysql-community-server
+systemctl enable mysqld
 systemctl start mysqld
+#mysql_secure_installation 重置数据库密码(ssh运行)
+
+#防火墙设置
 firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --reload
 #systemctl stop firewalld
 #systemctl disable firewalld
+firewall-cmd --reload
 
+#安装Python pip
 yum -y install python36-devel
 yum -y install python36
 curl -O https://bootstrap.pypa.io/get-pip.py -k
@@ -33,5 +62,7 @@ pip install pqi
 pqi use tuna
 pip install virtualenv
 pip install virtualenvwrapper
-pip install glances
-python3 -V && pip -V
+
+echo "$===================================================================================";
+
+nginx -v && mysql -V && python3 -V && pip -V
